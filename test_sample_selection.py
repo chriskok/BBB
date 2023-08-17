5db0855b3371564b9038974e83f238481f115318
chriskok <pherkok@gmail.com>
2023-08-17 16:59:26 -0400

    Finished full prompt testing pipeline

--- test_sample_selection.py
+++ test_sample_selection.py
@@ -15,6 +15,7 @@ from my_secrets import my_secrets
 openai_key = my_secrets.get('openai_key')
 openai.api_key = openai_key
 
+# --------------------------------------------- SAMPLING METHODS ---------------------------------------------
 # further AND closest items from mean
 def outlier_score(df):
     sentences = df["answer_text"].apply(str).tolist()
@@ -87,6 +88,7 @@ def cluster(df, n_clusters=10):
     df['cluster'] = clustering_model.labels_
     return df
 
+# --------------------------------------------- FILE WRITING & PROMPTING ---------------------------------------------
 def write_to_file(df, filename):
     # write the top N answer_texts and outlier_scores to a file
     with open(filename, 'w') as f:
@@ -120,6 +122,11 @@ def write_to_file_cluster(df, filename, sample=1):
                 f.write(str(row['cluster']) + '\t')
                 f.write('\n')
 
+def write_responses_to_file(title, response, filename):
+    with open(filename, 'a') as file:  # Open the file in append mode
+        uppercase_title = title.upper()
+        file.write(f"{uppercase_title}\n\n" + response + '\n\n---------------------------------------------------------------------------\n\n')
+
 def prompt_gpt4(prompt):
     model="gpt-4"
     try:
@@ -137,6 +144,14 @@ def prompt_gpt4(prompt):
         print(e)
         return "ERROR"
 
+# Placeholder function for GPT prompting
+def prompt_gpt(system_prompt=None, prompt_text=""):
+    if (system_prompt): msgs = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt_text}]
+    else: msgs = [{"role": "user", "content": prompt_text}]
+    response = prompt_gpt4(msgs)
+    return response
+
+# --------------------------------------------- PROMPT PHRASING ---------------------------------------------
 def prompt_phrasing(df, filename):
     prompts = [
         "Using the examples provided from a dataset, suggest potential rubric items that would be effective for evaluating students' answers.",
@@ -169,11 +184,74 @@ def prompt_phrasing(df, filename):
             uppercase_prompt = prompt.upper()
             file.write(f"{uppercase_prompt}\n\n" + response + '\n\n---------------------------------------------------------------------------\n\n')
 
-def write_responses_to_file(title, response, filename):
-    with open(filename, 'a') as file:  # Open the file in append mode
-        uppercase_title = title.upper()
-        file.write(f"{uppercase_title}\n\n" + response + '\n\n---------------------------------------------------------------------------\n\n')
+# --------------------------------------------- SAMPLE SIZE ---------------------------------------------
+def sample_answers(df, n_samples=1):
+    samples = []
+    num_clusters = df['cluster'].nunique()
+    for cluster_id in range(num_clusters): 
+        sample = df[df['cluster'] == cluster_id].sample(n=n_samples)
+        for index, row in sample.iterrows():
+            # remove new lines from answer_text
+            new_text = row['answer_text'].replace('\n', ' ')
+            # append to samples
+            samples.append(new_text)
+    return samples
 
+def sample_size_prompt(df, file_location):
+    rubric_prompt = "You are an expert instructor for your given course. You're in the process of designing a rubric to evaluate the short-answer, open-ended question: 'Describe why we want to use asynchronous programming in Javascript?' for a recent final exam. Using the examples provided from a dataset, suggest potential rubric items that would be effective for evaluating students' answers."
+    theme_prompt = "You are an expert instructor for your given course. You're in the process of designing a rubric to evaluate the short-answer, open-ended question: 'Describe why we want to use asynchronous programming in Javascript?' for a recent final exam. Based on the provided answers, identify the main themes or recurrent topics that students emphasized."
+    # sample 1 answer from each cluster - total 20
+    samples = sample_answers(df, n_samples=1)
+    user_prompt = "\n\n".join(samples)
+    response = prompt_gpt(rubric_prompt, user_prompt)
+    write_responses_to_file("Rubrics - 20 Answers", response, file_location + 'sample_size_prompts.txt')
+    response = prompt_gpt(theme_prompt, user_prompt)
+    write_responses_to_file("Themes - 20 Answers", response, file_location + 'sample_size_prompts.txt')
+    time.sleep(60)
+    # sample 2 answers from each cluster - total 40
+    samples = sample_answers(df, n_samples=2)
+    user_prompt = "\n\n".join(samples)
+    response = prompt_gpt(rubric_prompt, user_prompt)
+    write_responses_to_file("Rubrics - 40 Answers", response, file_location + 'sample_size_prompts.txt')
+    response = prompt_gpt(theme_prompt, user_prompt)
+    write_responses_to_file("Themes - 40 Answers", response, file_location + 'sample_size_prompts.txt')
+    time.sleep(60)
+    # sample 4 answers from each cluster - total 80
+    samples = sample_answers(df, n_samples=4)
+    user_prompt = "\n\n".join(samples)
+    response = prompt_gpt(rubric_prompt, user_prompt)
+    write_responses_to_file("Rubrics - 80 Answers", response, file_location + 'sample_size_prompts.txt')
+    response = prompt_gpt(theme_prompt, user_prompt)
+    write_responses_to_file("Themes - 80 Answers", response, file_location + 'sample_size_prompts.txt')
+    time.sleep(60)
+
+# --------------------------------------------- NUMBER OF RUBRICS/THEMES ---------------------------------------------
+def num_rubrics_prompt(df, file_location, n=3):
+    # generate several rounds of 3/5/10/X rubrics or themes to analyze the different outcomes
+    rubric_prompt = f"You are an expert instructor for your given course. You're in the process of designing a rubric to evaluate the short-answer, open-ended question: 'Describe why we want to use asynchronous programming in Javascript?' for a recent final exam. Using the examples provided from a dataset, suggest potential rubric items that would be effective for evaluating students' answers.\n\nCome up with a list of {n} rubric items that effectively encapsulate the types of answers in the dataset."
+    theme_prompt = f"You are an expert instructor for your given course. You're in the process of designing a rubric to evaluate the short-answer, open-ended question: 'Describe why we want to use asynchronous programming in Javascript?' for a recent final exam. Based on the provided answers, identify the main themes or recurrent topics that students emphasized.\n\nCome up with a list of {n} themes that effectively encapsulate the types of answers in the dataset."
+    samples = sample_answers(df, n_samples=1)  # total 20 answers
+    user_prompt = "\n\n".join(samples)
+    response = prompt_gpt(rubric_prompt, user_prompt)
+    write_responses_to_file(f"Rubrics - {n} items", response, file_location + 'num_rubrics_prompts.txt')
+    response = prompt_gpt(theme_prompt, user_prompt)
+    write_responses_to_file(f"Themes - {n} items", response, file_location + 'num_rubrics_prompts.txt')
+
+# --------------------------------------------- SAMPLE SELECTION ---------------------------------------------
+def sample_selection_prompt(df, file_location, method_name, clustered=True):
+    rubric_prompt = f"You are an expert instructor for your given course. You're in the process of designing a rubric to evaluate the short-answer, open-ended question: 'Describe why we want to use asynchronous programming in Javascript?' for a recent final exam. Using the examples provided from a dataset, suggest potential rubric items that would be effective for evaluating students' answers.\n\nCome up with a list of 5 rubric items that effectively encapsulate the types of answers in the dataset."
+    theme_prompt = f"You are an expert instructor for your given course. You're in the process of designing a rubric to evaluate the short-answer, open-ended question: 'Describe why we want to use asynchronous programming in Javascript?' for a recent final exam. Based on the provided answers, identify the main themes or recurrent topics that students emphasized.\n\nCome up with a list of 5 themes that effectively encapsulate the types of answers in the dataset."
+    if (clustered): samples = sample_answers(df, n_samples=1)  # total 20 answers
+    else: 
+        samples = df['answer_text'].tolist()
+        samples = samples[:20]  # total 20 answers
+    user_prompt = "\n\n".join(samples)
+    response = prompt_gpt(rubric_prompt, user_prompt)
+    write_responses_to_file(f"Rubrics - {method_name}", response, file_location + 'sample_selection_prompt.txt')
+    response = prompt_gpt(theme_prompt, user_prompt)
+    write_responses_to_file(f"Themes - {method_name}", response, file_location + 'sample_selection_prompt.txt')
+
+# --------------------------------------------- NUMBER OF CLUSTERS SHOWN ---------------------------------------------
 def full_context_prompt(df, file_location, num_samples=1):
     prompts = []
     num_clusters = df['cluster'].nunique()
@@ -207,13 +285,7 @@ def x_clusters_prompt(df, x, file_location, num_samples_per_cluster=5):
     full_response = all_rubrics + "\n\n" + response
     write_responses_to_file(f"{x} Clusters Prompt", full_response, file_location + 'cluster_size_prompts.txt')
 
-# Placeholder function for GPT prompting
-def prompt_gpt(system_prompt=None, prompt_text=""):
-    if (system_prompt): msgs = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt_text}]
-    else: msgs = [{"role": "user", "content": prompt_text}]
-    response = prompt_gpt4(msgs)
-    return response
-
+# --------------------------------------------- NEGATIVE/POSITIVE SCENARIOS ---------------------------------------------
 def manual_scenario(answers, file_location):
     # GPT generates themes
     themes = prompt_gpt(f"Generate themes based on these answers: \n\n{answers}")
@@ -259,9 +331,10 @@ def auto_no_theme_scenario(answers, file_location):
     rubrics = prompt_gpt(f"You are an expert instructor for your given course. Currently, you are evaluating student responses to the question: 'Describe why we want to use asynchronous programming in Javascript?' from a recent final exam. Generate 5 positive (common good answers) and 5 negative (potential misunderstandings) rubric items based on the given answers. Please output in the following format: - <rubric title>: <rubric description kept to 15 words maximum> (e.g. <example answer from the dataset provided>)", f"DATASET:\n\n{answers}")
     write_responses_to_file("Auto No Theme Scenario", rubrics, file_location + 'negative_rubric_generation_scenarios.txt')
 
-# questions = Question.objects.all()
-questions = Question.objects.filter(id=35)
+questions = Question.objects.all()
+# questions = Question.objects.filter(id=35)
 for q in questions:
+    print(q)
     file_location = f"results/chi_evaluations/question_{q.question_exam_id}/" + time.strftime("%Y%m%d-%H%M%S") + "/"
     # create directory if it doesn't exist
     if not os.path.exists(file_location):
@@ -270,25 +343,45 @@ for q in questions:
     # --------------------------------------------- PROMPT PHRASING ---------------------------------------------
     cluster20 = cluster(df, n_clusters=20)
     prompt_phrasing(cluster20, file_location + 'prompt_evaluations.txt')
+    time.sleep(60)
     # --------------------------------------------- SAMPLE SIZE ---------------------------------------------
+    cluster20 = cluster(df, n_clusters=20)
+    sample_size_prompt(cluster20, file_location)
+    time.sleep(60)
     # --------------------------------------------- NUMBER OF RUBRICS ---------------------------------------------
+    cluster20 = cluster(df, n_clusters=20)
+    num_rubrics_list = ["3", "5", "10", ""]
+    for n in num_rubrics_list:
+        num_rubrics_prompt(cluster20, file_location, n=n)
+        time.sleep(60)
     # --------------------------------------------- SAMPLE SELECTION ---------------------------------------------
-    # outlier_df = outlier_score(df)
+    outlier_df = outlier_score(df)
+    sample_selection_prompt(outlier_df, file_location, method_name="Outlier Score", clustered=False)
     # write_to_file(outlier_df, file_location + 'outlier_score.txt')
-    # further_df = outlier_score_furthest(df)
+    further_df = outlier_score_furthest(df)
+    sample_selection_prompt(further_df, file_location, method_name="Furthest from Mean", clustered=False)
     # write_to_file(further_df, file_location + 'outlier_score_furthest.txt')
-    # closer_df = outlier_score_closest(df)
+    closer_df = outlier_score_closest(df)
+    sample_selection_prompt(closer_df, file_location, method_name="Closest to Mean", clustered=False)
+    time.sleep(60)
     # write_to_file(closer_df, file_location + 'outlier_score_closest.txt')
-    # cluster10 = cluster(df, n_clusters=10)
+    cluster10 = cluster(df, n_clusters=10)
+    sample_selection_prompt(cluster10, file_location, method_name="Cluster 10")
     # write_to_file_cluster(cluster10, file_location + 'cluster10.txt', sample=2)
-    # cluster20 = cluster(df, n_clusters=20)
+    cluster20 = cluster(df, n_clusters=20)
+    sample_selection_prompt(cluster20, file_location, method_name="Cluster 20")
     # write_to_file_cluster(cluster20, file_location + 'cluster20.txt', sample=1)
-    # --------------------------------------------- SHOWN CLUSTERS ---------------------------------------------
-    # cluster20 = cluster(df, n_clusters=20)
-    # full_context_prompt(cluster20, file_location, num_samples=1)
-    # x_clusters_prompt(cluster20, x=2, file_location, num_samples_per_cluster=5)
-    # x_clusters_prompt(cluster20, x=5, file_location, num_samples_per_cluster=5)
-    # x_clusters_prompt(cluster20, x=10, file_location, num_samples_per_cluster=5)
+    time.sleep(60)
+    # --------------------------------------------- # SHOWN CLUSTERS ---------------------------------------------
+    cluster20 = cluster(df, n_clusters=20)
+    full_context_prompt(cluster20, file_location, num_samples=1)
+    time.sleep(60)
+    x_clusters_prompt(cluster20, x=2, file_location=file_location, num_samples_per_cluster=5)
+    time.sleep(60)
+    x_clusters_prompt(cluster20, x=5, file_location=file_location, num_samples_per_cluster=5)
+    time.sleep(60)
+    x_clusters_prompt(cluster20, x=10, file_location=file_location, num_samples_per_cluster=5)
+    time.sleep(60)
     # --------------------------------------------- FULL PIPELINE ---------------------------------------------
     cluster20 = cluster(df, n_clusters=20)
     samples = []
